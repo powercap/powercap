02f1900753cb7cd4aab165f3aa3c84df2c3442d2
Connor Imes <connor.k.imes@gmail.com>
2019-11-09 10:54:08 -0500

    powercap-rapl-sysfs: use 'zone' instead of 'pkg' as param names
    Improves generality - some newer platforms have multiple die/pkg

--- inc/powercap-rapl-sysfs.h
+++ inc/powercap-rapl-sysfs.h
@@ -3,11 +3,11 @@
  * This is a wrapper around powercap-sysfs.h.
  *
  * The control type is "intel-rapl" and zone depth is limited to 2.
- * The "pkg" parameters below are for the top-level "zone", and the optional "sz" parameters are for other control
+ * The "zone" parameters below are for the top-level "zone", and the optional "sz" parameters are for other control
  * planes like "core", "uncore", and "dram".
  * The "is_sz" parameter must be non-zero when working with these control planes.
  *
- * For example, pkg=0, sz=0, is_sz=1 is usually for the "core" power plane and is analogous to using powercap-sysfs.h
+ * For example, zone=0, sz=0, is_sz=1 is usually for the "core" power plane and is analogous to using powercap-sysfs.h
  * with zones[2]={0, 0}, depth=2.
  *
  * @author Connor Imes
@@ -25,153 +25,153 @@ extern "C" {
 #include <unistd.h>
 
 /**
- * Determine if a package exists.
+ * Determine if a top-level (parent) zone exists.
  *
- * @param pkg
- * @return 0 if package exists, a negative error code otherwise.
+ * @param zone
+ * @return 0 if zone exists, a negative error code otherwise.
  */
-int rapl_sysfs_pkg_exists(uint32_t pkg);
+int rapl_sysfs_pkg_exists(uint32_t zone);
 
 /**
  * Determine if a subzone exists.
  *
- * @param pkg
+ * @param zone
  * @param sz
  * @return 0 if subzone exists, a negative error code otherwise.
  */
-int rapl_sysfs_sz_exists(uint32_t pkg, uint32_t sz);
+int rapl_sysfs_sz_exists(uint32_t zone, uint32_t sz);
 
 /**
  * Determine if a constraint exists.
  *
- * @param pkg
+ * @param zone
  * @param sz
  * @param is_sz
  * @param constraint
  * @return 0 if constraint exists, a negative error code otherwise.
  */
-int rapl_sysfs_constraint_exists(uint32_t pkg, uint32_t sz, int is_sz, uint32_t constraint);
+int rapl_sysfs_constraint_exists(uint32_t zone, uint32_t sz, int is_sz, uint32_t constraint);
 
 /**
  * Get max_energy_range_uj for a zone.
  *
- * @param pkg
+ * @param zone
  * @param sz
  * @param is_sz
  * @param val
  * @return 0 on success, a negative error code otherwise.
  */
-int rapl_sysfs_zone_get_max_energy_range_uj(uint32_t pkg, uint32_t sz, int is_sz, uint64_t* val);
+int rapl_sysfs_zone_get_max_energy_range_uj(uint32_t zone, uint32_t sz, int is_sz, uint64_t* val);
 
 /**
  * Get energy_uj for a zone.
  *
- * @param pkg
+ * @param zone
  * @param sz
  * @param is_sz
  * @param val
  * @return 0 on success, a negative error code otherwise.
  */
-int rapl_sysfs_zone_get_energy_uj(uint32_t pkg, uint32_t sz, int is_sz, uint64_t* val);
+int rapl_sysfs_zone_get_energy_uj(uint32_t zone, uint32_t sz, int is_sz, uint64_t* val);
 
 /**
  * Enable/disable a zone.
  *
- * @param pkg
+ * @param zone
  * @param sz
  * @param is_sz
  * @param val
  * @return 0 on success, a negative error code otherwise.
  */
-int rapl_sysfs_zone_set_enabled(uint32_t pkg, uint32_t sz, int is_sz, uint32_t val);
+int rapl_sysfs_zone_set_enabled(uint32_t zone, uint32_t sz, int is_sz, uint32_t val);
 
 /**
  * Get whether a zone is enabled or disabled.
  *
- * @param pkg
+ * @param zone
  * @param sz
  * @param is_sz
  * @param val
  * @return 0 on success, a negative error code otherwise.
  */
-int rapl_sysfs_zone_get_enabled(uint32_t pkg, uint32_t sz, int is_sz, uint32_t* val);
+int rapl_sysfs_zone_get_enabled(uint32_t zone, uint32_t sz, int is_sz, uint32_t* val);
 
 /**
  * Get name for a zone.
  *
- * @param pkg
+ * @param zone
  * @param sz
  * @param is_sz
  * @param buf
  * @param size
  * @return number of bytes read, a negative error code otherwise.
  */
-ssize_t rapl_sysfs_zone_get_name(uint32_t pkg, uint32_t sz, int is_sz, char* buf, size_t size);
+ssize_t rapl_sysfs_zone_get_name(uint32_t zone, uint32_t sz, int is_sz, char* buf, size_t size);
 
 /**
  * Set power_limit_uw for a constraint.
  *
- * @param pkg
+ * @param zone
  * @param sz
  * @param is_sz
  * @param constraint
  * @param val
  * @return 0 on success, a negative error code otherwise.
  */
-int rapl_sysfs_constraint_set_power_limit_uw(uint32_t pkg, uint32_t sz, int is_sz, uint32_t constraint, uint64_t val);
+int rapl_sysfs_constraint_set_power_limit_uw(uint32_t zone, uint32_t sz, int is_sz, uint32_t constraint, uint64_t val);
 
 /**
  * Get power_limit_uw for a constraint.
  *
- * @param pkg
+ * @param zone
  * @param sz
  * @param is_sz
  * @param constraint
  * @param val
  * @return 0 on success, a negative error code otherwise.
  */
-int rapl_sysfs_constraint_get_power_limit_uw(uint32_t pkg, uint32_t sz, int is_sz, uint32_t constraint, uint64_t* val);
+int rapl_sysfs_constraint_get_power_limit_uw(uint32_t zone, uint32_t sz, int is_sz, uint32_t constraint, uint64_t* val);
 
 /**
  * Set time_window_us for a constraint.
  *
- * @param pkg
+ * @param zone
  * @param sz
  * @param is_sz
  * @param constraint
  * @param val
  * @return 0 on success, a negative error code otherwise.
  */
-int rapl_sysfs_constraint_set_time_window_us(uint32_t pkg, uint32_t sz, int is_sz, uint32_t constraint, uint64_t val);
+int rapl_sysfs_constraint_set_time_window_us(uint32_t zone, uint32_t sz, int is_sz, uint32_t constraint, uint64_t val);
 
 /**
  * Get time_window_us for a constraint.
  *
- * @param pkg
+ * @param zone
  * @param sz
  * @param is_sz
  * @param constraint
  * @param val
  * @return 0 on success, a negative error code otherwise.
  */
-int rapl_sysfs_constraint_get_time_window_us(uint32_t pkg, uint32_t sz, int is_sz, uint32_t constraint, uint64_t* val);
+int rapl_sysfs_constraint_get_time_window_us(uint32_t zone, uint32_t sz, int is_sz, uint32_t constraint, uint64_t* val);
 
 /**
  * Get max_power_uw for a constraint.
  *
- * @param pkg
+ * @param zone
  * @param sz
  * @param is_sz
  * @param constraint
  * @param val
  * @return 0 on success, a negative error code otherwise.
  */
-int rapl_sysfs_constraint_get_max_power_uw(uint32_t pkg, uint32_t sz, int is_sz, uint32_t constraint, uint64_t* val);
+int rapl_sysfs_constraint_get_max_power_uw(uint32_t zone, uint32_t sz, int is_sz, uint32_t constraint, uint64_t* val);
 
 /**
  * Get name for a constraint.
  *
- * @param pkg
+ * @param zone
  * @param sz
  * @param is_sz
  * @param constraint
@@ -179,7 +179,7 @@ int rapl_sysfs_constraint_get_max_power_uw(uint32_t pkg, uint32_t sz, int is_sz,
  * @param size
  * @return number of bytes read, a negative error code otherwise.
  */
-ssize_t rapl_sysfs_constraint_get_name(uint32_t pkg, uint32_t sz, int is_sz, uint32_t constraint, char* buf, size_t size);
+ssize_t rapl_sysfs_constraint_get_name(uint32_t zone, uint32_t sz, int is_sz, uint32_t constraint, char* buf, size_t size);
 
 #ifdef __cplusplus
 }
